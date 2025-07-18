716d8a2e54d2fdac9d94e5abe00e314bc23f5d71
Mostlydef <v.danilevitch2013@yandex.ru>
2025-07-18 22:02:30 +0300

    добавил миграции

---
+++ Infrastructure/Migrations/20250717162648_InitialCreate.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Games",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    BoardSize = table.Column<int>(type: "integer", nullable: false),
+                    WinCondition = table.Column<int>(type: "integer", nullable: false),
+                    Board = table.Column<string>(type: "text", nullable: false),
+                    Status = table.Column<int>(type: "integer", nullable: false),
+                    PlayerX = table.Column<Guid>(type: "uuid", nullable: false),
+                    PlayerO = table.Column<Guid>(type: "uuid", nullable: false),
+                    PlayerTurn = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Games", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Moves",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    GameId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Player = table.Column<Guid>(type: "uuid", nullable: false),
+                    Row = table.Column<int>(type: "integer", nullable: false),
+                    Column = table.Column<int>(type: "integer", nullable: false),
+                    Symbol = table.Column<int>(type: "integer", nullable: false),
+                    Timestamp = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Moves", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Moves_Games_GameId",
+                        column: x => x.GameId,
+                        principalTable: "Games",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Moves_GameId_Row_Column",
+                table: "Moves",
+                columns: new[] { "GameId", "Row", "Column" },
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Moves");
+
+            migrationBuilder.DropTable(
+                name: "Games");
+        }
+    }
+}
